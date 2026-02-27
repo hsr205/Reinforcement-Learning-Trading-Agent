e0a96c2fd604b4fd5e9e6ffb3ea73e24c01dbcdd
Henry Rothenberg <hsr205@aol.com>
2026-02-27 16:04:00 -0500

    WIP

--- trading_account/alpaca_trading_environment.py
+++ trading_account/alpaca_trading_environment.py
@@ -27,9 +27,8 @@ from utils.constants import Constants
 class AlpacaTradingEnvironment:
     ObsType: TypeVar = TypeVar("ObsType")
 
-    def __init__(self, ticker_symbol_str: str) -> None:
+    def __init__(self) -> None:
         self._api_key: str = settings.api_key
-        self._ticker_symbol_str: str = ticker_symbol_str
         self._bar_queue: queue.Queue[dict] = queue.Queue()
         self._bar_history: deque[dict] = deque(maxlen=5000)
         self._latest_bar_dict: dict[str, Any] | None = None
@@ -52,48 +51,68 @@ class AlpacaTradingEnvironment:
         # thread-safe handoff to your RL consumer loop
         self._bar_queue.put(bar_dict)
 
-    async def execute_trading_environment(self) -> None:
+    # TODO: Implement this method
+    def step(self, action_str: str) -> tuple[ObsType, Tensor, bool, bool, dict[str, Any]]:
+        pass
+
+        # return observation, reward_tensor, terminated, truncated, _
+
+    async def initialize_trading_environment(self) -> None:
 
         data_stream: StockDataStream = StockDataStream(api_key=self._api_key, secret_key=self._api_secret_key)
+        self.logger.info("=" * 100)
+        self.logger.info("Initializing Trading Environment")
 
         try:
 
-            # TODO: Use the following when ready to change multiple equities -> "TSLA", "AAPL", "META", "AMZN", "MSFT", "NVDA", "GOOGL"
-
-            data_stream.subscribe_bars(self._handle_bar, "AAPL")
+            # TODO: In order to test, minimize the amount of equities being traded
+            data_stream.subscribe_bars(self._handle_bar, "AAPL", "TSLA", "META", "AMZN", "MSFT", "NVDA", "GOOGL")
 
             stream_task: Task = asyncio.create_task(asyncio.to_thread(data_stream.run))
+
+            current_time_step: int = 1
 
             while True:
 
                 portfolio_list: list = self._trading_client.get_all_positions()
 
+                self._populate_portfolio(portfolio_list=portfolio_list)
+
                 state_data_dict: dict = await asyncio.to_thread(self._bar_queue.get) | self.get_market_features_dict(
                     portfolio_list=portfolio_list)
 
-                # # TODO: Use for testing
-                # test_dict: dict[str, Any] = {"symbol": "AAPL",
-                #                              "cash": 100_000}
+                self._execute_action_to_balance_portfolio(state_data_dict=state_data_dict)
 
-                random_action: OrderSide | str = self._get_random_action_from_action_space()
+                random_action_order_side_action: OrderSide | str = self._get_random_order_side_action()
 
-                if random_action == "HOLD":
+                if random_action_order_side_action != "HOLD":
+
+                    self.logger.info(f"Timestep Num: {current_time_step}")
+
+                    portfolio_cash:float = state_data_dict.get("cash")
+                    portfolio_equity:float = state_data_dict.get("equity")
+
+                    self.logger.info(f"Portfolio Equity: {portfolio_equity:,2f} -> Cash On Hand: ${portfolio_cash:,.2f}")
+                    self.logger.info("=" * 150)
+
+
+                else:
+                    self.logger.info(f"Action Selected -> {random_action_order_side_action}")
                     continue
 
-                random_quantity: int = self._get_random_quantity(state_data_dict=state_data_dict,
-                                                                 portfolio_list=portfolio_list,
-                                                                 random_action=random_action)
+                random_quantity_dict: dict[
+                    str, tuple[int, float, OrderSide]] = self._get_random_quantity_per_symbol_dict(
+                    state_data_dict=state_data_dict,
+                    portfolio_list=portfolio_list)
 
-                self.logger.info(f"random_action = {random_action}")
-                self.logger.info(f"random_quantity = {random_quantity}")
-                self.logger.info("=" * 100)
-
-                self.execute_random_action(quantity=random_quantity, random_action=random_action)
+                self.execute_random_action(random_quantity_dict=random_quantity_dict)
 
                 timestamp_utc: datetime = state_data_dict.get("timestamp")
                 current_time_est: time = timestamp_utc.astimezone(ZoneInfo("America/New_York")).time()
 
                 if current_time_est >= self._close_of_market_time:
+                    self.logger.info(f"Broken at timestep: {current_time_step}")
+                    self.logger.info("=" * 200)
                     break
 
                 # <-- THIS is where you build your (s, a, r, s') transition
@@ -102,106 +121,187 @@ class AlpacaTradingEnvironment:
                 # a  = agent.act(s)
                 # r  = reward(...)
                 # s2 = next_state(...)
+                current_time_step += 1
 
             await stream_task  # (won't reach in infinite loop unless you break)
 
         except Exception as e:
             self.logger.error(f"Exception Thrown: {e}")
 
-    def _get_random_action_from_action_space(self) -> OrderSide | str:
+    def _get_random_order_side_action(self) -> OrderSide | str:
         return random.choice(self._action_space)
 
-    def _get_random_quantity(self, state_data_dict: dict[str, Any], portfolio_list: list,
-                             random_action: OrderSide | str) -> int:
+    def _get_random_quantity_per_symbol_dict(self, state_data_dict: dict[str, Any], portfolio_list: list, ) -> dict[
+        str, tuple[int, float, OrderSide]]:
 
-        current_symbol: str = state_data_dict.get("symbol")
-        current_cash_t: float = state_data_dict.get("cash")
+        current_cash_t: float = float(state_data_dict.get("cash", 0.0))
 
-        for element in portfolio_list:
-            portfolio_symbol: str = element.symbol
+        random_quantity_dict: dict[str, tuple[int, float, OrderSide]] = {}
 
-            if portfolio_symbol != current_symbol:
+        for stock_data_collection in portfolio_list:
+
+            ticker_symbol_str: str = stock_data_collection.symbol
+            individual_stock_data_dict: dict[str, int | float] = state_data_dict.get(stock_data_collection.symbol, "")
+
+            current_stock_quantity: int = int(individual_stock_data_dict.get("qty_available", 0))
+            current_stock_price: float = float(individual_stock_data_dict.get("current_price", 0.0))
+
+            order_side: OrderSide = random.choice(Constants.ORDER_SIDE_ACTIONS_LIST)
+
+            is_buy_side: bool = order_side == OrderSide.BUY
+            is_sell_side: bool = order_side == OrderSide.SELL
+
+            if is_sell_side:
+                max_valid_quantity: int = current_stock_quantity
+
+                if max_valid_quantity <= 0:
+                    random_quantity_dict[ticker_symbol_str] = (0, current_stock_price, order_side)
+                    continue
+
+                random_quantity: int = random.randint(1, max_valid_quantity)
+                random_quantity_dict[ticker_symbol_str] = (random_quantity, current_stock_price, order_side)
                 continue
 
-            current_quantity: int = int(element.qty)
-            current_price: float = float(element.current_price)
+            if is_buy_side:
+                max_valid_quantity = int(current_cash_t // current_stock_price)
 
-            is_sell_order: bool = random_action == OrderSide.SELL
-            is_buy_order: bool = random_action == OrderSide.BUY
+                if max_valid_quantity <= 0:
+                    random_quantity_dict[ticker_symbol_str] = (0, current_stock_price, order_side)
+                    transaction_cost: float = current_stock_price * max_valid_quantity
+                    self.logger.warning(
+                        f"Invalid {order_side.name} of {current_stock_quantity:,} share(s) of {ticker_symbol_str}:"
+                    )
+                    self.logger.warning(
+                        f"Cash On Hand -> ${current_cash_t:,.2f}, Current Stock Price -> ${current_stock_price:,.2f}, Transaction Cost -> ${transaction_cost:,.2f}")
+                    continue
 
-            if is_sell_order:
-                max_valid_quantity: int = current_quantity
-                return self._get_random_int(max_valid_quantity=max_valid_quantity)
+                random_quantity = random.randint(1, max_valid_quantity)
 
-            if is_buy_order:
-                max_valid_quantity: int = int(current_cash_t // current_price)
-                return self._get_random_int(max_valid_quantity=max_valid_quantity)
+                # (Optional) sanity affordability check using the *actual* random_quantity
+                transaction_cost: float = current_stock_price * random_quantity
+                if transaction_cost > current_cash_t:
+                    random_quantity_dict[ticker_symbol_str] = (0, current_stock_price, order_side)
+                    self.logger.warning(
+                        f"Invalid {order_side.name} of {random_quantity:,} share(s) 0f {ticker_symbol_str}:"
+                    )
+                    self.logger.warning(
+                        f"Quantity -> {random_quantity}, Transaction Cost ->${transaction_cost:,.2f} exceeds Cash On Hand ->${current_cash_t:,.2f}")
+                    continue
 
-        return 0
+                random_quantity_dict[ticker_symbol_str] = (random_quantity, current_stock_price, order_side)
 
-    def _get_random_int(self, max_valid_quantity: int) -> int:
-        if max_valid_quantity <= 0:
-            return 0
-        return random.randint(1, max_valid_quantity)
+        return random_quantity_dict
 
-    def execute_random_action(self, quantity: float, random_action: OrderSide | str) -> None:
+    def execute_random_action(self, random_quantity_dict: dict[str, tuple[int, float, OrderSide]]) -> None:
 
         try:
 
-            market_order_request: MarketOrderRequest = MarketOrderRequest(
-                symbol=self._ticker_symbol_str,
-                qty=quantity,
-                side=random_action,
-                order_type=OrderType.MARKET,
-                time_in_force=TimeInForce.DAY
-            )
+            for ticker_symbol_str, ticker_symbol_tuple in random_quantity_dict.items():
 
-            market_order: Order = self._trading_client.submit_order(
-                order_data=market_order_request
-            )
+                stock_quantity: int = ticker_symbol_tuple[0]
+                stock_purchase_price: float = float(ticker_symbol_tuple[1])
+                stock_action: OrderSide = ticker_symbol_tuple[2]
 
-            self.logger.info(
-                f"Successful {market_order.side.name} {market_order.qty} share(s) of {market_order.symbol}")
+                if stock_quantity <= 0:
+                    continue
+
+                market_order_request: MarketOrderRequest = MarketOrderRequest(
+                    symbol=ticker_symbol_str,
+                    qty=stock_quantity,
+                    side=stock_action,
+                    order_type=OrderType.MARKET,
+                    time_in_force=TimeInForce.DAY
+                )
+
+                market_order: Order = self._trading_client.submit_order(
+                    order_data=market_order_request
+                )
+
+                market_order_qty_int: int = int(market_order.qty)
+
+                self.logger.info(
+                    f"Successfully {market_order.side.name} {market_order_qty_int} share(s) of {market_order.symbol} @ ${stock_purchase_price:,.2f} for ${stock_purchase_price * market_order_qty_int:,.2f}")
+
+            self.logger.info("=" * 100)
 
         except Exception as e:
             self.logger.warning(f"Exception Thrown: {e}")
 
-    def execute_action(self, quantity: int, action_type: OrderSide) -> None:
+    def _populate_portfolio(self, portfolio_list: list) -> None:
 
         try:
+            if not portfolio_list:
 
-            market_order_request: MarketOrderRequest = MarketOrderRequest(
-                symbol=self._ticker_symbol_str,
-                qty=quantity,
-                side=action_type,
-                order_type=OrderType.MARKET,
-                time_in_force=TimeInForce.DAY
-            )
+                self.logger.info("Populating Empty Portfolio")
+                self.logger.info("=" * 100)
 
-            market_order: Order = self._trading_client.submit_order(
-                order_data=market_order_request
-            )
+                for ticker_symbol_str in Constants.TICKER_SYMBOL_LIST:
+                    market_order_request: MarketOrderRequest = MarketOrderRequest(
+                        symbol=ticker_symbol_str,
+                        qty=1,
+                        side=OrderSide.BUY,
+                        order_type=OrderType.MARKET,
+                        time_in_force=TimeInForce.DAY
+                    )
 
-            self.logger.info(
-                f"Successful {market_order.side.name} {market_order.qty} share(s) of {market_order.symbol}")
+                    market_order: Order = self._trading_client.submit_order(
+                        order_data=market_order_request
+                    )
+
+                    self.logger.info(
+                        f"Successful {market_order.side.name} {market_order.qty} share(s) of {market_order.symbol}")
 
         except Exception as e:
             self.logger.warning(f"Exception Thrown: {e}")
 
-    def _get_ticker_data_dict(self, portfolio_list: list) -> dict[str, Any]:
+    def _execute_action_to_balance_portfolio(self, state_data_dict: dict) -> None:
 
-        for position in portfolio_list:
+        try:
 
-            if position.symbol == self._ticker_symbol_str:
-                ticker_data_dict: dict[str, Any] = {
-                    "quantity": position.qty,
-                    "qty_available": position.qty_available,
-                    "cost_basis": position.cost_basis,
-                    "current_price": position.current_price,
-                    "change_today": position.change_today,
-                }
+            for key, value in state_data_dict.items():
 
-                return ticker_data_dict
+                if isinstance(value, dict):
+
+                    ticker_quantity: int = int(value.get("quantity"))
+                    ticker_qty_available: int = int(value.get("qty_available"))
+
+                    if ticker_quantity < 0 or ticker_qty_available < 0:
+                        quantity_to_buy = abs(ticker_quantity)
+
+                        market_order_request: MarketOrderRequest = MarketOrderRequest(
+                            symbol=key,
+                            qty=quantity_to_buy,
+                            side=OrderSide.BUY,
+                            order_type=OrderType.MARKET,
+                            time_in_force=TimeInForce.DAY
+                        )
+
+                        market_order: Order = self._trading_client.submit_order(
+                            order_data=market_order_request
+                        )
+
+                        self.logger.info(
+                            f"Successful {market_order.side.name} {market_order.qty} share(s) of {market_order.symbol}")
+
+        except Exception as e:
+            self.logger.warning(f"Exception Thrown: {e}")
+
+    def _get_ticker_data_dict(self, portfolio_list: list) -> dict[str, dict[str, Any]]:
+
+        ticker_data_dict: dict[str, dict[str, Any]] = {}
+
+        for index, position in enumerate(portfolio_list):
+            data_dict: dict[str, float] = {
+                "quantity": position.qty,
+                "qty_available": position.qty_available,
+                "cost_basis": position.cost_basis,
+                "current_price": position.current_price,
+                "change_today": position.change_today,
+            }
+
+            ticker_data_dict[position.symbol] = data_dict
+
+        return ticker_data_dict
 
     def _get_account_data_dict(self) -> dict[str, float]:
         try:
@@ -229,41 +329,15 @@ class AlpacaTradingEnvironment:
         except Exception as e:
             self.logger.error(f"Exception thrown: {e}")
 
-    def step(self, action_str: str) -> tuple[ObsType, Tensor, bool, bool, dict[str, Any]]:
-        pass
 
-        # return observation, reward_tensor, terminated, truncated, _
 
-    def get_state_dict(self) -> dict[str, Any]:
-
-        state_dict: dict[str, Any] = {}
-        individual_stock_data_list: list[HistoricalStockDataObj] = self.get_individual_stock_data_list()
-
-        market_features_dict: dict[str, float] = self.get_market_features_dict()
-
-        for data_obj in individual_stock_data_list:
-            self.logger.info(f"data_obj.observation_num = {data_obj.observation_num}")
-            self.logger.info(f"data_obj.ticker_symbol_id = {data_obj.ticker_symbol_id}")
-            self.logger.info(f"data_obj.timestamp = {data_obj.timestamp}")
-            self.logger.info(f"data_obj.open = {data_obj.open}")
-            self.logger.info(f"data_obj.close = {data_obj.close}")
-            self.logger.info(f"data_obj.high = {data_obj.high}")
-            self.logger.info(f"data_obj.low = {data_obj.low}")
-            self.logger.info(f"data_obj.volume = {data_obj.volume}")
-            break
-
-        for key, value in market_features_dict.items():
-            self.logger.info(f"key = {key} -> value = {value}")
-
-        return state_dict
-
-    def get_market_features_dict(self, portfolio_list: list) -> dict[str, float]:
+    def get_market_features_dict(self, portfolio_list: list) -> dict[str, Any]:
 
         account_data_dict: dict[str, Any] = self._get_account_data_dict()
-        ticker_data_dict: dict[str, Any] = self._get_ticker_data_dict(portfolio_list=portfolio_list)
-        ticker_state_dict: dict[str, Any] = account_data_dict | ticker_data_dict
+        ticker_data_dict: dict[str, dict[str, Any]] = self._get_ticker_data_dict(portfolio_list=portfolio_list)
+        market_features_dict: dict[str, Any] = account_data_dict | ticker_data_dict
 
-        return ticker_state_dict
+        return market_features_dict
 
     def get_individual_stock_data_list(self) -> list[HistoricalStockDataObj]:
 
