37dbcc30cc88900b94ac9a33b4ccad92aaa929dd
Henry Rothenberg <hsr205@aol.com>
2026-03-01 19:59:50 -0500

    Add fields to config.py module

--- config/config.py
+++ config/config.py
@@ -3,8 +3,11 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    api_key: str = Field(..., description="API key for Alpaca Markets Connection")
-    api_secret_key: str = Field(..., description="API secret key for Alpaca Markets Connection")
+    api_key_random: str = Field(..., description="API key for connection to Random_Trading_Account on Alpaca Markets")
+    api_secret_key_random: str = Field(..., description="API secret key for connection to Random_Trading_Account on Alpaca Markets")
+
+    api_key_ppo: str = Field(..., description="API key for connection to PPO_Trading_Account on Alpaca Markets")
+    api_secret_key_ppo: str = Field(..., description="API secret key for connection to PPO_Trading_Account on Alpaca Markets")
 
     model_config = SettingsConfigDict(
         env_file=".env",
